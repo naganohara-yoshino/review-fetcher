417d1de19933dea628a097ccf5e9d4e8b94e9181
naganohara-yoshino <naganohara-yoshino@outlook.com>
2026-02-01 19:53:42 +0800

    feat: Entry

---
+++ src/lib/components/PresentedSubject.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import type { PresentedSubject } from '$lib/schemas/presented-subject';
+
+	let { presentedSubject }: { presentedSubject: PresentedSubject } = $props();
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Field from '$lib/components/ui/field/index.js';
+	const id = $props.id();
+</script>
+
+<Card.Root class="mx-auto w-full max-w-sm">
+	<Card.Header>
+		<Card.Title class="text-2xl">Get Reviews</Card.Title>
+		<Card.Description>Enter your username below to get your public anime reviews</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<form>
+			<Field.Group>
+				<Field.Field>
+					<Field.Label for="username-{id}">Username</Field.Label>
+					<Input id="username-{id}" placeholder="sai" required />
+				</Field.Field>
+				<Field.Field>
+					<Button type="submit" class="w-full">Go</Button>
+				</Field.Field>
+			</Field.Group>
+		</form>
+	</Card.Content>
+</Card.Root>
