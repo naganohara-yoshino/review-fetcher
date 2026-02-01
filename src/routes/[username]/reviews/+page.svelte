96f30cfc223b959e97d48495ba5ebd15700b8384
naganohara-yoshino <naganohara-yoshino@outlook.com>
2026-02-01 19:30:51 +0800

    feat: shadcn init

---
+++ src/routes/[username]/reviews/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+	let { presentedSubjects } = data;
+</script>
+
+<div>
+  {#each presentedSubjects as presentedSubject }
+    <div>{presentedSubject.comment}</div>
+  {/each}
+</div>
